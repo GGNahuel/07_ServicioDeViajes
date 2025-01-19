440f98135673023fc4ccab17c4933e51eb033dc9
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-01-19 17:39:38 -0300

    innit backend project

---
+++ backend/src/main/java/com/nahuelgDev/journeyjoy/JourneyJoyApiApplication.java
@@ -0,0 +1,13 @@
+package com.nahuelgDev.journeyjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyJoyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyJoyApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nahuelgDev/journeyjoy/JourneyJoyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahuelgDev.journeyjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyJoyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
