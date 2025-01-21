26e6cc663c76bbe6cc3d3f3ce0a4f85d371dc531
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-01-21 14:36:28 -0300

    create search query in travels repository

--- backend/src/main/java/com/nahuelgDev/journeyjoy/repositories/TravelsRepository.java
+++ backend/src/main/java/com/nahuelgDev/journeyjoy/repositories/TravelsRepository.java
@@ -1,11 +1,28 @@
 package com.nahuelgDev.journeyjoy.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.nahuelgDev.journeyjoy.collections.Travels;
 
 @Repository
 public interface TravelsRepository extends MongoRepository<Travels, String> {
-  
+  @Query("{ $and: [ " +
+    "{ $or: [ { :#{#available} : null }, { isAvailable: :#{#available} } ] }, " +
+    "{ $or: [ { $expr: { $gte: [ :#{#desiredCapacity}, {$subtract: ['$maxCapacity', '$currentCapacity']} ] } }, { :#{#desiredCapacity} : null } ] }, " +
+    "{ $or: [ { 'destinies.place': :#{#place} }, { :#{#place} : '' } ] }, " +
+    "{ longInDays: { $lte: :#{#maxDays} } }, " +
+    "{ longInDays: { $gte: :#{#minDays} } } " +
+  "]}")
+  List<Travels> search(
+    @Param("available") Boolean available, 
+    @Param("desiredCapacity") Integer desiredCapacity,
+    @Param("place") String place,
+    @Param("maxDays") String maxDays,
+    @Param("minDays") String minDays
+  );
 }
