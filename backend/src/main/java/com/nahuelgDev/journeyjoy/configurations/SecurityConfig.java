8f978f908f57f40a6959f88427f1cb94b93fb50d
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-01-26 21:45:55 -0300

    add spring security dependency and configuration

---
+++ backend/src/main/java/com/nahuelgDev/journeyjoy/configurations/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.nahuelgDev.journeyjoy.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nahuelgDev.journeyjoy.utilities.Generators;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+  @Bean
+  SecurityFilterChain setFilterChainProps(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+      .authorizeHttpRequests(request -> request
+        .anyRequest().permitAll()
+      )
+      .formLogin(form -> form
+        .loginProcessingUrl("/logincheck")
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .successHandler((request, response, authentication) -> {
+          response.setStatus(HttpServletResponse.SC_OK);
+        })
+        .permitAll()
+      )
+      .logout(logout -> logout
+        .logoutUrl("/logout")
+        .logoutSuccessHandler((request, response, asd) -> {
+          response.setStatus(HttpServletResponse.SC_OK);
+        })
+        .permitAll()
+      )
+      .rememberMe(remember -> remember
+        .key(Generators.generateKey(16))
+        .tokenValiditySeconds(43200)
+      );
+
+    return httpSecurity.build();
+  }
+
+  @Bean
+  BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
---
+++ backend/src/main/java/com/nahuelgDev/journeyjoy/utilities/Generators.java
@@ -0,0 +1,17 @@
+package com.nahuelgDev.journeyjoy.utilities;
+
+import java.security.SecureRandom;
+
+public class Generators {
+  public static String generateKey(int length) {
+    SecureRandom random = new SecureRandom();
+    StringBuilder sb = new StringBuilder(length);
+    String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    
+    for (int i = 0; i < length; i++) {
+      sb.append(characters.charAt(random.nextInt(characters.length())));
+    }
+    
+    return sb.toString();
+  }
+}
