2d7003adea1e7a913d1c355ca4b188a028789d48
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-03-09 17:49:38 -0300

    remove use of state in Card component

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,15 +1,17 @@
 import { css, SerializedStyles } from "@emotion/react";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 
 export function Card({children, additionalStyles, onHover} : {children: ReactNode, additionalStyles?: SerializedStyles, onHover?: () => void}) {
-  const [isHovered, setIsHovered] = useState(false)
   const style = css`
-    box-shadow: ${!isHovered ? 
-      "3px 3px 3px rgba(57, 57, 57, 0.5), -1px -1px 3px rgba(57, 57, 57,0.5)" 
-      : "5px 5px 6px 2px rgba(57, 57, 57,0.4), -1px -1px 6px 1px rgba(57, 57, 57,0.4)"};
+    box-shadow: 3px 3px 3px rgba(57, 57, 57, 0.5), -1px -1px 3px rgba(57, 57, 57,0.5);
     border-radius: 16px;
     padding: 1rem;
     transition: box-shadow 100ms;
+
+    &:hover {
+      box-shadow: 5px 5px 6px 2px rgba(57, 57, 57,0.4), -1px -1px 6px 1px rgba(57, 57, 57,0.4);
+    }
+
     ${additionalStyles || ""}
   `
 
@@ -17,11 +19,7 @@ export function Card({children, additionalStyles, onHover} : {children: ReactNod
     <article 
       onMouseEnter={() => {
         if (onHover) onHover()
-        setIsHovered(true)
       }} 
-      onMouseLeave={
-        () => setIsHovered(false)
-      }
       css={style}
     >
       {children}
