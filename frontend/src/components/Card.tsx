3ebf58991f2c40f4454a7f499329a07d8d70452b
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-03-09 14:18:24 -0300

    change de background of cards in benefits section, from gradient to image. use of emotion in CardComponent

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,23 +1,29 @@
-import { CSSProperties, ReactNode, useState } from "react";
+import { css, SerializedStyles } from "@emotion/react";
+import { ReactNode, useState } from "react";
 
-export function Card({children, styles, onHover} : {children: ReactNode, styles?: CSSProperties, onHover?: () => void}) {
+export function Card({children, additionalStyles, onHover} : {children: ReactNode, additionalStyles?: SerializedStyles, onHover?: () => void}) {
   const [isHovered, setIsHovered] = useState(false)
+  const style = css`
+    box-shadow: ${!isHovered ? 
+      "3px 3px 3px rgba(57, 57, 57, 0.5), -1px -1px 3px rgba(57, 57, 57,0.5)" 
+      : "5px 5px 6px 2px rgba(57, 57, 57,0.4), -1px -1px 6px 1px rgba(57, 57, 57,0.4)"};
+    border-radius: 16px;
+    padding: 1rem;
+    transition: box-shadow 100ms;
+    ${additionalStyles || ""}
+  `
 
   return (
-    <article onMouseEnter={() => {
-      if (onHover) onHover()
-      setIsHovered(true)
-    }} onMouseLeave={
-      () => setIsHovered(false)
-    } style={{
-      boxShadow: !isHovered ? 
-        "3px 3px 3px rgba(57, 57, 57, 0.5), -1px -1px 3px rgba(57, 57, 57,0.5)" 
-        : "5px 5px 6px 2px rgba(57, 57, 57,0.4), -1px -1px 6px 1px rgba(57, 57, 57,0.4)",
-      borderRadius: "16px",
-      padding: "1rem",
-      transition: "box-shadow 100ms",
-      ...styles,
-    }} className="cards">
+    <article 
+      onMouseEnter={() => {
+        if (onHover) onHover()
+        setIsHovered(true)
+      }} 
+      onMouseLeave={
+        () => setIsHovered(false)
+      }
+      css={style}
+    >
       {children}
     </article>
   )
--- frontend/src/pages/indexSections/Benefits.tsx
+++ frontend/src/pages/indexSections/Benefits.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react"
 import { Card } from "../../components/Card"
 import { useWindowProps } from "../../hooks/useWindowsProps"
+import { css } from "@emotion/react"
 
 export function Benefits() {
   const {width} = useWindowProps()
@@ -15,12 +16,12 @@ export function Benefits() {
         <img src="/maleta.jpg" alt="" style={{width: "100%"}} />
         <BenefitCardContainer side="left">
           <BenefitCard name="Planes Personalizados" />
-          <BenefitCard name="Asesoramiento Experto" />
-          <BenefitCard name="Asistencia 24/7" />
+          <BenefitCard name="Asesoramiento Experto" variation />
+          <BenefitCard name="Asistencia 24/7" variation />
           {width > 930 && <>
             <BenefitCard name="Flexibilidad en Pagos" />
             <BenefitCard name="Seguros de Viaje Incluidos" />
-            <BenefitCard name="Reservas Seguras y Rápidas" />
+            <BenefitCard name="Reservas Seguras y Rápidas" variation />
           </>}
         </BenefitCardContainer>
         {width <= 930 && <BenefitCardContainer side="right">
@@ -34,32 +35,44 @@ export function Benefits() {
 }
 
 function BenefitCardContainer({side, children} : {side: "left" | "right", children: ReactNode}) {
+  const style = css`
+    position: absolute;
+    width: 36%;
+    height: 68%;
+    top: 14%;
+    left: ${side == "left" ? "7.5" : "52.5"}%;
+    padding: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+  `
+
   return (
-    <div style={{
-      position: "absolute",
-      width: "36%",
-      height: "68%",
-      top: "14%",
-      left: side == "left" ? "7.5%" : "52.5%",
-      padding: "1rem",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-around"
-    }}>
+    <div css={style}>
       {children}
     </div>
   )
 }
 
-function BenefitCard({name, desc}: {name: string, desc?: string}) {
+function BenefitCard({name, variation}: {name: string, variation?: boolean}) {
+  const style = css`
+    display: flex;
+    align-items: center;
+    color: rgb(54, 150, 72);
+    background-image: url("${variation ? "rollo2.png" : "rollo.png"}");
+    background-size: 100% 100%;
+    width: 100%;
+
+    h3 {
+      border-radius: 8px;
+      padding: 8px;
+      background: rgba(255, 255, 255, 0.75);
+    }
+  `
+
   return (
-    <Card styles={{
-      display: "flex",
-      alignItems: "center",
-      color: "aliceblue"
-    }}>
+    <Card additionalStyles={style}>
       <h3>{name}</h3>
-      <p>{desc}</p>
     </Card>
   )
 }
