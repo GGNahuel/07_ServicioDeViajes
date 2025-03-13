d9ab36d569800ceb48aaaa9614488af94e9bc83d
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-03-13 13:29:23 -0300

    rename request handler and make the context call outside it

--- frontend/src/requests/_RequestHandler.tsx
+++ frontend/src/requests/_RequestHandler.tsx
@@ -1,6 +1,5 @@
-import { useContext } from "react"
 import { API_PREFIX } from "../const/ApiConstants"
-import { MessageContext, MessageContextInterface } from "../contexts/MessageContext"
+import { MessageContextInterface } from "../contexts/MessageContext"
 import { ApiMethods } from "../types/ApiTypes"
 import { buildRequestBody } from "../utils/buildRequestBody"
 import { CriticalError, WarnError } from "../utils/errorClasses"
@@ -13,15 +12,14 @@ export async function handleRequest(
   path: string, method: ApiMethods, {headers, objectToStringify, alternativeBody} : {
     headers?: ReqBodyHeaders,
     objectToStringify?: unknown, alternativeBody?: BodyInit
-  }
+  }, contextSetter: MessageContextInterface["setValue"]
 ) {
-  const {setValue} = useContext(MessageContext) as MessageContextInterface
-
   const requestBody = buildRequestBody({method, headers, alternativeBody, objectToStringify})
 
   try {
     const request = await fetch(API_PREFIX + path, requestBody)
     const response = await request.json()
+    console.log(request, response)
 
     if (!request.ok) {
       if (request.status == 500)
@@ -29,15 +27,15 @@ export async function handleRequest(
       throw new WarnError(response)
     }
 
-    return { response }
+    return response 
   } catch (error) {
     if (error instanceof CriticalError)
-      setValue({
+      contextSetter({
         message: (error as Error).message,
         type: "error"
       })
     else
-      setValue({
+      contextSetter({
         message: (error as Error).message,
         type: "warn"
       })
