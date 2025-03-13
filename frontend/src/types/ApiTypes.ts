2e4dbad9d2c6c4a43f661a39aec46c205326d98e
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-03-13 13:29:53 -0300

    create types based in API

--- frontend/src/types/ApiTypes.ts
+++ frontend/src/types/ApiTypes.ts
@@ -1,7 +1,92 @@
 export type ApiMethods = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
 
+enum Transports { PLANE, BUS, TRAIN, BOAT }
+enum PayPLansType { friends, family, couple, individual }
+
 export interface PopularTravel {
   name: string,
   description: string,
   placesNames: string[]
+}
+
+export interface Travel {
+  id?: string,
+  name: string,
+  longInDays: number, 
+  maxCapacity: number,
+  currentCapacity?: number,
+  isAvailable: boolean,
+  availableDates: string[],
+  rating?: number,
+
+  destinies: Destiny[],
+  payPlans: PayPlan[],
+  reviews: Review[],
+}
+
+interface Destiny {
+  place: string,
+  leaveDay: number,
+  returnDay: number,
+  transport: Transports,
+
+  stayPlaceId: string
+}
+
+interface PayPlan {
+  price: number,
+  planFor: PayPLansType
+}
+
+export interface Review {
+  id?: string,
+  userName: string,
+  comment: string,
+  rating: number,
+
+  userImage: Image
+}
+
+export interface Image {
+  id: string,
+  name: string,
+  contentType: string,
+  data: string
+}
+
+export interface StayPlace {
+  id?: string,
+  from: string,
+  name: string,
+  description?: string
+  rating: number
+}
+
+export interface Request {
+  id?: string,
+  selectedPlan: PayPLansType,
+  persons: Person,
+  email: {
+    id: string,
+    email: string,
+    owner: string
+  },
+  amountPaid: number,
+  totalPrice: number,
+
+  associatedTravel: string,
+  selectedDate: string
+}
+
+interface Person {
+  name: string,
+  age: number,
+  identificationNumber: number,
+  contactPhone: number
+}
+
+export interface User {
+  id?: string,
+  username: string,
+  password: string
 }
