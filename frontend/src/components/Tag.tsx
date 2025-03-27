eb289f975f0bef70ac7f97f4cf1a3157b6fb0036
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-03-27 13:14:46 -0300

    create tag component

---
+++ frontend/src/components/Tag.tsx
@@ -0,0 +1,24 @@
+import { css, SerializedStyles } from "@emotion/react";
+import { ReactNode } from "react";
+
+export function Tag({children, variant, additionalStyles} : {children: ReactNode, variant?: "default" | "empty", additionalStyles?: SerializedStyles}) {
+  const style = css`
+    padding: 0.2rem;
+    font-weight: 600;
+    border-radius: 16px;
+    display: flex;
+    align-items: center;
+    ${variant === "empty" ? 
+      "border: 1px solid rgb(10, 110, 110);" : 
+      "background-color: var(--mainColor); color: white;"
+    }
+
+    ${additionalStyles}
+  `
+  
+  return (
+    <span css={style}>
+      {children}
+    </span>
+  )
+}
