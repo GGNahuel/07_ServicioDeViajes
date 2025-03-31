885f4c11131657e4342e33af88cb124f777fb0d3
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-03-30 21:38:28 -0300

    create make request page with functions to add persons in cards

---
+++ frontend/src/pages/requests/MakeRequestPage.tsx
@@ -0,0 +1,104 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Button } from "../../components/Button";
+import { MainSection } from "../../components/MainSection";
+import { AddIcon, CheckIcon, PencilIcon, TrashCanIcon } from "../../components/SvgIcons";
+import { Person, Travel } from "../../types/ApiTypes";
+import { formatDate } from "../../utils/fromApi/formatDateFromApi";
+import { Card } from "../../components/Card";
+import { css } from "@emotion/react";
+
+// type actionsToPersonList = "add" | "edit" | "delete"
+type PersonCardType = {id: number, data: Person | null}
+
+export function MakeRequestPage({travel} : {travel?: Travel}) {
+  const [cardList, setCardList] = useState<PersonCardType[]>([])
+
+  const handleChangesInPersonsAdded = (e: FormEvent<HTMLFormElement>, card: PersonCardType) => {
+    e.preventDefault()
+    
+    const newEntry = {
+      id: card.id,
+      data: card.data
+    }
+
+    setCardList(prev => [...prev, newEntry])
+  }
+
+  return (
+    <MainSection>
+      <h2>Formulario para {travel?.name}</h2>
+      <form>
+        <label>Ingrese su email: <input type="email" name="email" /></label>
+        <label>Seleccione una de las fechas disponibles<select name="date">
+          {travel?.availableDates.map(date => <option value={formatDate(date)}></option>)}  
+        </select></label>
+
+        <section>
+          <header>
+            <h3>Ingrese los datos de las personas que viajaran</h3>
+            <Button variant="rounded" onClick={() => {
+              const id = cardList.length > 0 ? cardList[cardList.length - 1].id + 1 : 0
+              setCardList(prev => [...prev, {id, data: null}])
+            }}><AddIcon /></Button>
+          </header>
+          {cardList.map(card => <PersonCardInRequestForm key={card.id} card={card} handleAction={handleChangesInPersonsAdded} />)}
+        </section>
+      </form>
+    </MainSection>
+  )
+}
+
+function PersonCardInRequestForm(
+  {handleAction, card} : 
+  {handleAction: (e: FormEvent<HTMLFormElement>, card: PersonCardType) => void, card: PersonCardType}
+) {
+  const [showingForm, _] = useState<boolean>(card.data != null)
+  const [personInForm, setPersonInForm] = useState<Person>({
+    name: "",
+    age: -1,
+    identificationNumber: 0,
+    contactPhone: 0
+  })
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>, property: keyof Person) => {
+    setPersonInForm(prev => ({
+      ...prev,
+      [property]: e.target.value
+    }))
+  }
+
+  return (
+    <Card>
+      {showingForm && <form onSubmit={e => handleAction(e, {id: card.id, data: personInForm})}>
+        <label>Nombre completo
+          <input type="text" name="name" value={personInForm.name} onChange={(e) => handleInputChange(e, "name")}/></label>  
+        <label>Edad
+          <input type="number" name="age" value={personInForm.age} onChange={(e) => handleInputChange(e, "age")} />
+        </label>
+        <label>Número de identificación
+          <input type="number" name="idNumber" value={personInForm.identificationNumber} onChange={e => handleInputChange(e, "identificationNumber")} />
+        </label>
+        <label>Número de contacto
+          <input type="number" name="contactPhone" value={personInForm.contactPhone} onChange={e => handleInputChange(e, "contactPhone")} />
+        </label>
+        <Button variant={"default"} type="submit" additionalStyles={css`gap: 8px; align-items: center;`}
+          disabled={personInForm.name === "" || personInForm.age >= 0}
+        >
+          <CheckIcon/>Agregar
+        </Button>
+      </form>}
+      {!showingForm && card.data && <>
+        <div>
+          <Button variant="rounded"><PencilIcon /></Button>
+          <Button variant="rounded"><TrashCanIcon /></Button>
+        </div>
+        <div>
+          <p><strong>Nombre completo</strong> {card.data.name}</p>
+          <p><strong>Edad</strong> {card.data.age}</p>
+          <p><strong>Número de identificación</strong> {card.data.identificationNumber}</p>
+          <p><strong>Número de contacto</strong> {card.data.contactPhone}</p>
+        </div>
+      </>}
+    </Card>
+  )
+}
