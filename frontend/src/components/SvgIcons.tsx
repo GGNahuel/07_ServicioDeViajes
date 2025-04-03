a91a387d8153a491683458bc6c345f4ac7da2012
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-04-02 21:21:38 -0300

    fix trash can icon and make css in them

--- frontend/src/components/SvgIcons.tsx
+++ frontend/src/components/SvgIcons.tsx
@@ -1,6 +1,13 @@
+import { css } from "@emotion/react"
+
+const styles = css`
+  width: 24px;
+  height: 24px;
+`
+
 export function LocationSvg() {
   return (
-    <svg className="icon" version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enableBackground="new 0 0 64 64" xmlSpace="preserve" fill="#000000">
+    <svg className="icon" css={styles} version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enableBackground="new 0 0 64 64" xmlSpace="preserve" fill="#000000">
       <g strokeWidth="0"></g>
       <g strokeLinecap="round" strokeLinejoin="round"></g>
       <g> 
@@ -18,7 +25,7 @@ export function LocationSvg() {
 
 export function Close() {
   return (
-    <svg className="icon" fill="#9e0000" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 492 492" xmlSpace="preserve" stroke="#9e0000">
+    <svg className="icon" css={styles} fill="#9e0000" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 492 492" xmlSpace="preserve" stroke="#9e0000">
       <g strokeWidth="0"></g>
       <g strokeLinecap="round" strokeLinejoin="round"></g>
       <g> <g> <g> 
@@ -30,7 +37,7 @@ export function Close() {
 
 export function Next() {
   return (
-    <svg className="icon" fill="#000000" width="64px" height="64px" viewBox="0 0 24 24" id="next" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg">
+    <svg className="icon" css={styles} fill="#000000" width="64px" height="64px" viewBox="0 0 24 24" id="next" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg">
       <g strokeWidth="0"></g>
       <g strokeLinecap="round" strokeLinejoin="round"></g>
       <g>
@@ -42,7 +49,7 @@ export function Next() {
 
 export function Previous() {
   return (
-    <svg className="icon" fill="#000000" width="64px" height="64px" viewBox="0 0 24 24" id="next" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
+    <svg className="icon" css={styles} fill="#000000" width="64px" height="64px" viewBox="0 0 24 24" id="next" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
       <g strokeWidth="0"></g>
       <g strokeLinecap="round" strokeLinejoin="round"></g>
       <g>
@@ -54,7 +61,7 @@ export function Previous() {
 
 export function PencilIcon () {
   return (
-    <svg className="icon" fill="#000000" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg">
+    <svg className="icon" css={styles} fill="#000000" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg">
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
       <g id="SVGRepo_iconCarrier"> <title></title>
@@ -66,19 +73,23 @@ export function PencilIcon () {
 
 export function TrashCanIcon () {
   return (
-    <svg className="icon" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4 6H20L18.4199 20.2209C18.3074 21.2337 17.4512 22 16.4321 22H7.56786C6.54876 22 5.69264 21.2337 5.5801 20.2209L4 6Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M7.34491 3.14716C7.67506 2.44685 8.37973 2 9.15396 2H14.846C15.6203 2 16.3249 2.44685 16.6551 3.14716L18 6H6L7.34491 3.14716Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M2 6H22" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M10 11V16" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M14 11V16" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <svg className="icon" css={styles} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier"> 
+        <path d="M4 6H20L18.4199 20.2209C18.3074 21.2337 17.4512 22 16.4321 22H7.56786C6.54876 22 5.69264 21.2337 5.5801 20.2209L4 6Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> 
+        <path d="M7.34491 3.14716C7.67506 2.44685 8.37973 2 9.15396 2H14.846C15.6203 2 16.3249 2.44685 16.6551 3.14716L18 6H6L7.34491 3.14716Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> 
+        <path d="M2 6H22" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> 
+        <path d="M10 11V16" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> 
+        <path d="M14 11V16" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> 
+      </g>
     </svg>
   )
 }
 
 export function AddIcon() {
   return (
-    <svg className="icon" fill="#000000" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlSpace="preserve">
+    <svg className="icon" css={styles} fill="#000000" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlSpace="preserve">
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
       <g id="SVGRepo_iconCarrier"> 
@@ -94,7 +105,7 @@ export function AddIcon() {
 
 export function CheckIcon() {
   return (
-    <svg className="icon" fill="#000000" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg">
+    <svg className="icon" css={styles} fill="#000000" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg">
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
       <g id="SVGRepo_iconCarrier"> <title></title> 
